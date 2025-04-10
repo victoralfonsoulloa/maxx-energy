d1f10f2d49cc6a0d6575ba5001c977428b4bc82b
Andrei Novozhilov <andreinovozhilov15@gmail.com>
2025-04-10 09:47:24 -0700

    fixed merge issues

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,22 @@
 import { useState } from 'react'
-
 import './App.css'
+import { NavBar } from './components/NavBar/NavBar.jsx'
 import Footer from './components/Footer/Footer.jsx'
 import MainContent  from './components/MainContent/MainContent.jsx'
 
+
 function App() {
 
-  return (
- 
- <div className='page-wrap'>
- <MainContent/>
- <Footer/>
 
-    </div>
+  return (
+    <>
+      <NavBar/>
+      <MainContent/>
+      <Footer/>
+
+    </>
   )
 }
+
 
 export default App
