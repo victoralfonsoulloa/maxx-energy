69c66ab79313714b765a0680a8a181e77f025dfd
Andrei Novozhilov <andreinovozhilov15@gmail.com>
2025-04-10 21:39:31 -0700

    about us page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,26 @@
-import { useState } from 'react'
-import './App.css'
-import { NavBar } from './components/NavBar/NavBar.jsx'
-import Footer from './components/Footer/Footer.jsx'
-import MainContent from './components/MainContent/MainContent.jsx'
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import { NavBar } from './components/NavBar/NavBar.jsx';
+import Footer from './components/Footer/Footer.jsx';
+import MainContent from './components/MainContent/MainContent.jsx';
+import AboutUs from './components/AboutUs.jsx';
+import DataPage from './components/DataPage'; 
 
 function App() {
   return (
-    <>
+    <Router>
       <NavBar />
-      <MainContent />
+
+      <Routes>
+        <Route path="/" element={<MainContent />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/data" element={<DataPage />} /> {/* optional */}
+      </Routes>
+
       <Footer />
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/DataPage.jsx
@@ -0,0 +1,12 @@
+export default function DataPage (){
+    return (
+        <div className="data-page">
+            <h1>MAXX Energy Dashboard</h1>
+            <div className="charts">
+                <div className="chart-card">Chart 1</div>
+                <div className="chart-card">Chart 2</div>
+            </div>
+
+        </div>
+    )
+}
